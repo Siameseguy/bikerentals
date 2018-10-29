1f3fec9057cd01638c6abf36b496dce386e1f8f1
siameseguy <psyhakhom@gmail.com>
2018-10-29 19:13:10 -0400

    fixed responsiveness of app

--- src/Cart.js
+++ src/Cart.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Cart(props) {
   return (
-    <div className="cart col-xs-12">
+    <div className="cart col-xs-12 col-md-4">
       <div className="cart-items">
         <button className="btn btn-success btn-lg btn-block">total: ${props.total}</button>
       </div>
